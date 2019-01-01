680ffc031f785a52a38818c54a774a54eafee3b6
Anh Tuan <atn832@gmail.com>
2019-01-01 15:10:51 +0900

    implemented rendering of one character and sum of its pixel values

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 An absolute bare-bones web app.
 
+## Running
+
+After getting dependencies with `pub get`, run:
+
+```
+webdev serve
+```
+
 Created from templates made available by Stagehand under a BSD-style
 [license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,22 @@
 import 'dart:html';
 
+CanvasElement canvas;  
+CanvasRenderingContext2D ctx;
+
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  canvas = querySelector('#canvas');
+  ctx = canvas.getContext('2d');
+  ctx.font = '50px serif';
+  ctx.textBaseline = 'top';
+  ctx.fillText('녕', 0, 0);
+  final sumOfValues = sum(ctx.getImageData(0, 0, 50, 50).data);
+  querySelector('#output').text = 'Sum of pixel values: $sumOfValues';
+}
+
+int sum(List<int> data) {
+  var total = 0;
+  data.forEach((n) {
+    total += n;
+  });
+  return total;
 }
